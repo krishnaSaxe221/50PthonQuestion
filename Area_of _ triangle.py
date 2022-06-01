15e38d6ef2d79cfc5b3f8551becc81c5eddd3835
krishna saxena <100754752+krishnaSaxe221@users.noreply.github.com>
2022-06-01 22:55:52 +0530

    Add files via upload

---
+++ Area_of _ triangle.py
@@ -0,0 +1,6 @@
+a=float(input("Enter first side of triangle :"))
+b=float(input("Enter second side of triangle :"))
+c=float(input("Enter third side of triangle :"))
+s=(a+b+c)/2
+a=(s*(s-a)*(s-b)*(s-c))**(1/2)
+print("Area of triangle is :%.2f"%a
